c9116acbfcdc3eb3e6f51e4129fb61ec471b8027
Noelsky <hansorly@ajou.ac.kr>
2022-02-23 03:32:57 +0900

    Feat :

--- client/src/Common/Constant/index.tsx
+++ client/src/Common/Constant/index.tsx
@@ -1,5 +1,38 @@
+import { DefaultBlackColor, DefaultWhiteColor } from "@Style/.";
+import { PAGES_INFO_TYPE } from "@Type/.";
+
 export const LOGO = `HWH`;
 
-export const PHONE_NUMBER = `+82 10-2980-9419`;
+export const PHONE_NUMBER = `+82 10-2980-9419 `;
 
-export const EMAIL = `WOOHEON@GMAIL.COM`;
+export const EMAIL = ` WOOHEON@GMAIL.COM`;
+
+export const IntroductionMessage = ["데이터로", "말하는", "분석가", "홍우헌"];
+
+export const LinkedInLink = "https://www.linkedin.com/in/wooheon-hong-b33621200/";
+
+export const GithubLink = "https://github.com/WooheonHong";
+
+export const PAGE_INFO: PAGES_INFO_TYPE = {
+  AboutMe: {
+    fontColor: DefaultBlackColor,
+  },
+  Career: {
+    fontColor: DefaultWhiteColor,
+  },
+  Experience: {
+    fontColor: DefaultBlackColor,
+  },
+  Introduction: {
+    fontColor: DefaultWhiteColor,
+  },
+  Learned: {
+    fontColor: DefaultBlackColor,
+  },
+  Project: {
+    fontColor: DefaultWhiteColor,
+  },
+  SkillSet: {
+    fontColor: DefaultBlackColor,
+  },
+};
