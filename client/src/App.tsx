c4d45c8e111c643e339bfdc0ed92af5f7f4ce8ac
Noelsky <hansorly@ajou.ac.kr>
2022-02-23 03:35:44 +0900

    Feat : 페이지 구조 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,22 +2,29 @@ import Header from "@Organisms/Header";
 import AboutMeTemplate from "@Templates/AboutMe";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { LOGO, PHONE_NUMBER, EMAIL } from "@Constant/.";
+import CareerTemplate from "@Templates/Career";
+import ExprienceTemplate from "@Templates/Experience";
+import IntroductionTemplate from "@Templates/Introduction";
+import LearnedTemplate from "@Templates/Learned";
+import ProjectTemplate from "@Templates/Project";
+import SkillSetTemplate from "@Templates/SkillSet";
+import MainPage from "./Components/Page";
 
 const App = () => {
   return (
-    <>
+    <MainPage>
       <Header logoText={LOGO} infoText={`${PHONE_NUMBER}/${EMAIL}`} />
       <Routes>
-        <Route path="/AboutMe" element={<AboutMeTemplate index={0} />} />
-        <Route path="/SkillSet" />
-        <Route path="/Career" />
-        <Route path="/Project" />
-        <Route path="/Exprience" />
-        <Route path="/Learned" />
-        <Route path="/Introduction" />
+        <Route path="/AboutMe" element={<AboutMeTemplate pageName="AboutMe" />} />
+        <Route path="/SkillSet" element={<SkillSetTemplate index={0} />} />
+        <Route path="/Career" element={<CareerTemplate index={0} />} />
+        <Route path="/Project" element={<ProjectTemplate index={0} />} />
+        <Route path="/Exprience" element={<ExprienceTemplate index={0} />} />
+        <Route path="/Learned" element={<LearnedTemplate index={0} />} />
+        <Route path="/Introduction" element={<IntroductionTemplate index={0} />} />
         <Route path="*" element={<Navigate replace to="/AboutMe" />} />
       </Routes>
-    </>
+    </MainPage>
   );
 };
 
