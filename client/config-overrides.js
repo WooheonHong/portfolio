89fd0f62e5c9d96bcb4b5eb63286870ecb48059a
Noelsky <hansorly@ajou.ac.kr>
2022-02-22 18:05:39 +0900

    Etc : alias 수정

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,25 +1,25 @@
 const {
-    addWebpackAlias,
-    override
+  addWebpackAlias,
+  override
 } = require("customize-cra");
 const path = require("path");
 
 const resolve = (src) => path.resolve(__dirname, src);
 
 module.exports = override(
-    addWebpackAlias({
-        "@": resolve("src"),
-        "@Pages": resolve("src/Components/Pages/"),
-        "@Atoms": resolve("src/Components/Atoms/"),
-        "@Molecules": resolve("src/Components/Molecules/"),
-        "@Organisms": resolve("src/Components/Organisms/"),
-        "@Templates": resolve("src/Components/Templates/"),
-        "@Constant": resolve("src/Common/Constant/"),
-        "@Style": resolve("src/Common/Style/"),
-        "@Util": resolve("src/Common/Util/"),
-        "@Type": resolve("src/Common/Type/"),
-        "@API": resolve("src/API/"),
-        "@Recoil": resolve("src/Recoil/"),
-        "@src": resolve("./"),
-    })
+  addWebpackAlias({
+    "@": resolve("src"),
+    "@Pages": resolve("src/Components/Pages"),
+    "@Atoms": resolve("src/Components/Atoms"),
+    "@Molecules": resolve("src/Components/Molecules"),
+    "@Organisms": resolve("src/Components/Organisms"),
+    "@Templates": resolve("src/Components/Templates"),
+    "@Constant": resolve("src/Common/Constant"),
+    "@Style": resolve("src/Common/Style"),
+    "@Util": resolve("src/Common/Util"),
+    "@Type": resolve("src/Common/Type"),
+    "@API": resolve("src/API"),
+    "@Hook": resolve("src/Hook"),
+    "@src": resolve("./"),
+  })
 );
