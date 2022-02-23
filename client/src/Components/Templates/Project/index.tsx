5897ee7cd6e125fed6d5c49174535f9ab761c41d
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-02-24 01:58:39 +0900

    Feat : 프로젝트 페이지 UI 완성

--- client/src/Components/Templates/Project/index.tsx
+++ client/src/Components/Templates/Project/index.tsx
@@ -1,7 +1,23 @@
+import { IdxReducer } from "@Hook/.";
+import ContentHeader from "@Organisms/ContentHeader";
+import { ContentContainer } from "@Templates/Career/style";
+import { useReducer } from "react";
+import { PAGE_INFO } from "@Constant/.";
+import ProjectContent from "@Organisms/ProjectContnet";
 import { Container } from "./style";
 
-const ProjectTemplate = () => {
-  return <Container>a</Container>;
+const ProjectTemplate = ({ pageName }: { pageName: string }) => {
+  const [index, setIndex] = useReducer(IdxReducer, 0);
+  const { fontColor } = PAGE_INFO[pageName];
+
+  return (
+    <Container>
+      <ContentContainer>
+        <ContentHeader color={fontColor} title="Project" index={index} setIndex={setIndex} />
+        <ProjectContent index={index} />
+      </ContentContainer>
+    </Container>
+  );
 };
 
 export default ProjectTemplate;
